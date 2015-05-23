b941dac3b0d01861c6cb2bcbf5aa47ead6d5c47f
liaualien <liaualien@gmail.com>
2015-05-23 10:22:31 +0800

    first commit

---
+++ 0523/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_0523.Startup))]
+namespace _0523
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
